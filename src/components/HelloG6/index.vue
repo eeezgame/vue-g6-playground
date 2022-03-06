415d18a04ad8411ce0f034a0a17c33632fba0192
eeezgame <eeezgame@163.com>
2022-03-06 20:25:43 +0800

    hello g6

---
+++ src/components/HelloG6/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>HelloG6</div>
+  <div ref="graph$"></div>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+import { useMyGraph } from "./useMyGraph.js";
+
+export default {
+  name: "HelloG6",
+  setup() {
+    const graph$ = ref(null);
+    let graphInstance = null;
+    onMounted(() => {
+      const { graph } = useMyGraph(graph$.value);
+      graphInstance = graph;
+      console.log("graphInstance:", graphInstance);
+    });
+    return { graph$ };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less"></style>
